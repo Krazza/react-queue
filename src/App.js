94adf9df12e3da0c84a08b3be1671c0004fdea78
Muondo Vladislav <s2200741@BCC02J6TQ6LRAir.lan>
2022-12-06 14:36:14 +0200

    queue done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    const [userInput, setUserInput] = useState();
+    const [queue, setQueue] = useState([]);
+
+    return(
+        <div className="container">
+            <h1>Queue</h1>
+            <h2>Result: {queue.join(", ")}</h2>
+            <input type="text" onChange={HandleInput}></input>
+            <div className="buttonContainer">
+                <button className="mybuttonclass" onClick={AddToQueue}>Add</button>
+                <button className="mybuttonclass" onClick={RemoveFromQueue}>Remove</button>
+            </div>
+        </div>
+    )
+
+    function HandleInput(event)
+    {
+        setUserInput(event.target.value);
+    }
+
+    function AddToQueue()
+    {
+        let tempArr = queue;
+        tempArr.push(userInput)
+        setQueue([...tempArr]);
+    }
+
+    function RemoveFromQueue()
+    {
+        let tempArr = queue;
+        tempArr.shift();
+        setQueue([...tempArr]);
+    }
 }
 
 export default App;
